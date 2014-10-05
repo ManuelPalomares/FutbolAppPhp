5746d5b26c93ff2d95fb204d16ce118d53f53ddc
ManuelPalomares <manuel936@gmail.com>
2014-10-05 10:49:04 -0500

    cabmbios

--- model/session.php
+++ model/session.php
@@ -6,7 +6,7 @@ class SessionApp {
     private function isRegisterUser() {
         //echo "__".$_SESSION["APP_WEBMAPSOFT.COM"]["USER"]."__";
         $user = $_SESSION["APP_WEBMAPSOFT.COM"]["USER"];
-        $user ="ADMIN";
+        //$user ="ADMIN";
         if (isset($user)) {
             return "S";
         } else {
@@ -27,12 +27,12 @@ class SessionApp {
                 $res["success"] = true;
                 $res["user"] =$_SESSION["APP_WEBMAPSOFT.COM"]["USER"] ;
                 $res["nombre"] =$_SESSION["APP_WEBMAPSOFT.COM"]["NOMBRE"] ;
-                $res["nombre"] = "Manuel Alejandro Palomares";
+                
                 echo json_encode($res);
                 exit();
             }else{
-                //return $_SESSION["APP_WEBMAPSOFT.COM"]["USER"];
-                return 'ADMIN';
+                return $_SESSION["APP_WEBMAPSOFT.COM"]["USER"];
+                //return 'ADMIN';
             }
                 
         }
