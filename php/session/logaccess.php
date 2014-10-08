55d1e0d978299649ba3ad726a8f816966173497d
ManuelPalomares <manuel936@gmail.com>
2014-10-07 20:46:36 -0500

    Se actualiza cerrar session
    se actualiza el cerrar sesion

--- php/session/logaccess.php
+++ php/session/logaccess.php
@@ -1,4 +1,5 @@
 <?php
+
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: X-Requested-With');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
@@ -9,32 +10,38 @@ require_once("../../model/usuarios.php");
 session_start();
 $datos = $_REQUEST;
 
-$accion  = $datos["accion"];
+$accion = $datos["accion"];
 $usuario_nombre = $datos['usuario'];
 $password = $datos['password'];
 
-$usuarios = new Usuarios($usuario_nombre,$accion,1);
-$usuario =  $usuarios->consultarUsuario($usuario_nombre, $password);
-
-if($usuario[0]["CODIGO"] != null){
+if($accion=="CERRARSESION"){
+    session_destroy();
+    echo json_encode(array("success"=>true)); 
     
-    if($usuario[0]['ESTADO']=='I'){
-        $res["success"] = true;
-        $res["mensaje_error"] = "Su usuario esta inactivo";
-        echo json_encode($res);
-        exit();
-    }
-    //REGISTRAR USUARIO
-    $_SESSION["APP_WEBMAPSOFT.COM"]["USER"] = $usuario[0]["USUARIO"];
-    $_SESSION["APP_WEBMAPSOFT.COM"]["NOMBRE"] = $usuario[0]["NOMBRE"];
-    $res["success"] = true;
-    echo json_encode($res);
+}
+if ($accion == "CONECTARSE") {
+    $usuarios = new Usuarios($usuario_nombre, $accion, 1);
+    $usuario = $usuarios->consultarUsuario($usuario_nombre, $password);
 
-}else{
+    if ($usuario[0]["CODIGO"] != null) {
+
+        if ($usuario[0]['ESTADO'] == 'I') {
+            $res["success"] = true;
+            $res["mensaje_error"] = "Su usuario esta inactivo";
+            echo json_encode($res);
+            exit();
+        }
+        //REGISTRAR USUARIO
+        $_SESSION["APP_WEBMAPSOFT.COM"]["USER"] = $usuario[0]["USUARIO"];
+        $_SESSION["APP_WEBMAPSOFT.COM"]["NOMBRE"] = $usuario[0]["NOMBRE"];
+        $_SESSION["APP_WEBMAPSOFT.COM"]["NOMBRE"] = $usuario[0]["NOMBRE"];
+        $res["success"] = true;
+        echo json_encode($res);
+    } else {
         $res["success"] = true;
         $res["mensaje_error"] = "Usuario o contraseña Invalida";
         echo json_encode($res);
         exit();
+    }
 }
-
 ?>
