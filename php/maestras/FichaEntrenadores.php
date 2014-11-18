ea6e09fa8c15669fd9082eafaf9a6e75c9fed6c4
aiobando <aiobando@gmail.com>
2014-11-17 19:45:24 -0500

    ficha entrenadores

--- php/maestras/FichaEntrenadores.php
+++ php/maestras/FichaEntrenadores.php
@@ -6,7 +6,7 @@ header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 
 require_once("../include/index.php"); 
 require_once ("../../model/session.php");
-require_once ("../../model/Jugadores.php");
+require_once ("../../model/Entrenadores.php");
 
 /* Controla el acceso a usuarios externos no logueados */
 $session = new SessionApp();
@@ -20,27 +20,27 @@ $codigo=$datos["codigo"];
 $estado=$datos["estado"];
 $tipo_documento=$datos["tipo_documento"];
 $doc_identidad=$datos["doc_identidad"];
-$fecha_expedicion=$datos["fecha_expedicion"];
+//$fecha_expedicion=$datos["fecha_expedicion"];
 $nombres=$datos["nombres"];
 $apellidos=$datos["apellidos"];
 $fecha_nacimiento=$datos["fecha_nacimiento"];
-$codigo_lugar_nacimiento=$datos["codigo_lugar_nacimiento"];
-$tipo_sangre=$datos["tipo_sangre"];
+//$codigo_lugar_nacimiento=$datos["codigo_lugar_nacimiento"];
+//$tipo_sangre=$datos["tipo_sangre"];
 $direccion=$datos["direccion"];
 $barrio=$datos["barrio"];
 $telefono=$datos["telefono"];
 $celular=$datos["celular"];
 $email=$datos["email"];
-$bb_pin=$datos["bb_pin"];
-$colegio=$datos["colegio"];
-$grado=$datos["grado"];
+//$bb_pin=$datos["bb_pin"];
+//$colegio=$datos["colegio"];
+//$grado=$datos["grado"];
 $genero=$datos["genero"];
-$seguridad_social=$datos["seguridad_social"];
-$codigo_categoria=$datos["codigo_categoria"];
-$codigo_suscriptor=$datos["codigo_suscriptor"];
+//$seguridad_social=$datos["email"];
+//$codigo_categoria=$datos["codigo_categoria"];
+//$codigo_suscriptor=$datos["codigo_suscriptor"];
 $observaciones=$datos["observaciones"];
 $foto = $datos["foto"];
-$imagen_jugador = $_FILES["imagenJugador"];
+$imagen_entrenador = $_FILES["imagenEntrenador"];
 
 //para el paginador Extjs
 $start       = $datos["start"];
@@ -53,9 +53,9 @@ $query  = $datos["query"];
 
 /*TODO Operaciones con las variables POST O GET*/
 //crear clase Roles
-$jugador = new Jugadores($usuario, $accion, $opcion_actual);
+$entrenador = new Entrenadores($usuario, $accion, $opcion_actual);
 if($accion == "GUARDAR"){
-    $rs = $jugador->guardarJugador($fecha_ingreso,$estado,$tipo_documento,$doc_identidad,$fecha_expedicion,$nombres,$apellidos,$fecha_nacimiento,$codigo_lugar_nacimiento,$tipo_sangre,$direccion,$barrio,$telefono,$celular,$email,$bb_pin,$colegio,$grado,$genero,$seguridad_social,$codigo_categoria,$codigo_suscriptor,$observaciones,$foto);    
+    $rs = $entrenador->guardarJugador($fecha_ingreso,$estado,$tipo_documento,$doc_identidad,$nombres,$apellidos,$fecha_nacimiento,$direccion,$barrio,$telefono,$celular,$email,$genero,$observaciones,$foto);    
     echo json_encode($rs);
     exit();
 }
